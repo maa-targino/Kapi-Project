aee3641f7381bc539a47a3d108d49c6d2289da08
Marco Targino <maa.targino@gmail.com>
2021-11-09 18:57:36 -0300

    mongodb connected with mongoose

---
+++ models/Deal.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+
+mongoose.model('Deal', {
+    //recebe um json
+})
--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const app = express()
-const pipedrive = require('pipedrive')
+const express = require('express') //import express
+const app = express() //initialize express
+const pipedrive = require('pipedrive') //import pipedrive api
 const mongoose = require('mongoose')
 
-app.listen(3000, () => {
-    console.log('Running')
+mongoose.connect('mongodb://kapi:A53nh4ML48@@kapidb.lrhg4.mongodb.net/KapiDB?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+} , () => {
+    console.log('Database connected')
+})
+
+app.listen(8080, () => {
+    console.log('Running API') //callback
 })
