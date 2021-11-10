01d75bd956c778355f69d026d6e93c54faa95773
Marco Targino <maa.targino@gmail.com>
2021-11-10 14:07:47 -0300

    successfully connected with mlab

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const router = express.Router()
-const Deal = require('../models/Deal')
+const Post = require ('../models/Post')
 
-router.get('/posts', (req, res) => {
+router.get('/', (req, res) => {
     res.send('This is our posts page')
 })
 
 router.post('/', (req, res) => {
-    console.log(req.body)
+    // console.log(req.body)
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    })
+
+    post.save().then((data) => {
+        res.json(data)
+        // res.statusCode = 201
+        // res.send()
+    }).catch((err) => {
+        // if(err) {
+        //     throw err
+        // }
+        res.json({message: err})
+    })
 })
 
 module.exports = router
--- src/app.js
+++ src/app.js
@@ -1,27 +1,27 @@
-const express = require('express') //import express
-const app = express() //initialize express
-const pipedrive = require('pipedrive') //import pipedrive api
+const express = require('express') //Import express
+const app = express() //Initialize express
 const mongoose = require('mongoose')
 require ('dotenv/config')
 
-//Import routes
+//Middleware
+app.use(express.json())
+
+//Import post route
 const posts = require('../routes/posts')
 
 app.use('/posts', posts)
 
-//enables app to receive json
-app.use(express.json())
-
 //Connect to DB
 mongoose.connect(
-    process.env.MONGODB_URI, { useNewUrlParser: true }, () => console.log('Connected to DB')
+    process.env.DB_CONNECT, { useNewUrlParser: true }, () => console.log('Connected to DB')
 )
 
-//endpoints
-app.get('/', (req, res) => {
-    res.send('This is our home page')
+//Callback
+app.listen(3000, () => {
+    console.log('Running API')
 })
 
-app.listen(8080, () => {
-    console.log('Running API') //callback
+//Endpoints:
+app.get('/', (req, res) => {
+    res.send('This is our home page')
 })
